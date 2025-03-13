1a97489979fab875ae18577df2ac89156f8c8544
zaqtec <61351197+zaqtec@users.noreply.github.com>
2025-03-13 16:48:36 +0000

    faschifo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,86 @@
-interface PokemonCard{
+import { Await } from "react-router";
+import { PokeAPI } from "./pokeapiClient";
+import { useEffect, useState } from "react"
+
+interface PokemonCard {
   id: number;
   image: string;
   name: string;
-  types: string;
+  type: string[];
+}
+ 
+async function fetchData(): Promise<string[]> {
+  const data = await PokeAPI.getPokemonsList();
+  return data.results.map(item => item.name);
+
 }
 
-export const App = () =>{
-  return <div>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
+const data = await fetchData
+
+const typeColors: { [key: string]: string } = {
+  fire : "bg-red-500",
+  water: "bg-blue-500",
+  fly: "bg-grey-500",
+  dragon: "bg-orange-500",
+};
+function getTypeColor(type: string) {
+  const color = typeColors[type];
+  return color;
+}
+
+const Card = (props: PokemonCard) => {
+ return (
+  <div>
+              {props.id} - {props.name}
+              <img src={props.image} alt={props.name} />
+              <div className="flex flex-wrap">
+                {props.type.map((type) => {
+                  return <div className={p-4 §{getTypeColor(type) }}>{type}</div>;
+                })}
+              </div>
+            </div>
+ )
+}
+export const App = () => {
+  const [data, setData] = useState<PokemonCard[]>([]);
+  
+  useEffect(() => {
+    fetchData().then((results) => {
+      setData (
+        results.map((item) => ({
+          id: 1,
+          name: item,
+          image: item,
+          type: [item],
+          }))
+      );
+    });
+
+  
+
+  }, []);
+  
+  return (
+    <div>
+      <div className="flex flex-wrap bg-white grid grid-cols-4">
+        {data.map((item) => {
+          return <Card
+          id={item.id}
+          name={item.name}
+          image={item.image}
+          type={item.type}
+          />
+            
+          
+        })}
+      </div>
     </div>
-}
+  );
+};
 
-export const Detail = () =>{
-  return null
-}
 
+    
+
+export const Detail = () => {
+ return null
+}
