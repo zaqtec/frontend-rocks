1989a44f41d4dec47dbab4f00f38cb7349ced609
zaqtec <61351197+zaqtec@users.noreply.github.com>
2025-03-13 09:22:44 +0000

    messaggio

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://giovannigiordano.github.io/frontend-rocks/' : '/frontend-rocks',
+  base: process.env.NODE_ENV === 'production' ? 'https://zaqtec.github.io/frontend-rocks/' : '/frontend-rocks',
   plugins: [react(), tailwindcss()],
 })
 
